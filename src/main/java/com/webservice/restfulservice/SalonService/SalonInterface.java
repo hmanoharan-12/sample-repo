c9348fdf8550e959ab5033c0e442fd28efa35398
Harry Manoharan <harrymanoharan1@gmail.com>
2019-10-12 11:20:07 +0530

    deleted files

--- src/main/java/com/webservice/restfulservice/SalonService/SalonInterface.java
+++
@@ -1,7 +0,0 @@
-package com.webservice.restfulservice.SalonService;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface SalonInterface extends CrudRepository<Salon, String>{
-
-}
